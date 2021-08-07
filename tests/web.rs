7f9bf8212462e2a153ee7b6626456f0cc81b1f4f
marshll <marshll692@gmail.com>
2021-08-07 17:09:40 +0300

    basic wireworld implementation

--- tests/web.rs
+++ tests/web.rs
@@ -3,11 +3,314 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
+use std::assert_eq;
+
 use wasm_bindgen_test::*;
+
+extern crate wasm_wireworld_rs;
+use wasm_wireworld_rs::{Cell, World};
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+fn test_new_world_created_empty() {
+    let mut world = World::new(2, 2);
+
+    let empty_cells = [
+        (0, 0, Cell::Empty),
+        (0, 1, Cell::ElectronHead),
+        (1, 0, Cell::Empty),
+        (1, 1, Cell::Empty),
+    ];
+
+    world.set_cells(&empty_cells);
+}
+
+// test wireworld rules
+
+#[wasm_bindgen_test]
+fn test_empty_stay_empty() {
+    let mut world = World::new(2, 2);
+    world.tick();
+
+    let expected = &[Cell::Empty, Cell::Empty, Cell::Empty, Cell::Empty];
+
+    assert_eq!(world.get_cells(), expected);
+}
+
+#[wasm_bindgen_test]
+fn test_empty_electron_head_become_tail() {
+    let mut world = World::new(2, 2);
+    world.set_cells(&[(0, 1, Cell::ElectronHead)]);
+    world.tick();
+
+    let expected = &[Cell::Empty, Cell::ElectronTail, Cell::Empty, Cell::Empty];
+
+    assert_eq!(world.get_cells(), expected);
+}
+
+#[wasm_bindgen_test]
+fn test_empty_electron_tail_become_conductor() {
+    let mut world = World::new(2, 2);
+    world.set_cells(&[(0, 1, Cell::ElectronTail)]);
+    world.tick();
+
+    let expected = &[Cell::Empty, Cell::Conductor, Cell::Empty, Cell::Empty];
+
+    assert_eq!(world.get_cells(), expected);
+}
+
+#[wasm_bindgen_test]
+fn test_conductor_become_electron_head_with_1_neighbour() {
+    let mut world = World::new(3, 3);
+    world.set_cells(&[(1, 1, Cell::Conductor), (0, 0, Cell::ElectronHead)]);
+    world.tick();
+
+    let expected = &[
+        Cell::ElectronTail, // 0 0
+        Cell::Empty,        // 0 1
+        Cell::Empty,        // 0 2
+        Cell::Empty,        // 1 0
+        Cell::ElectronHead, // 1 1
+        Cell::Empty,        // 1 2
+        Cell::Empty,        // 2 0
+        Cell::Empty,        // 2 1
+        Cell::Empty,        // 2 2
+    ];
+
+    assert_eq!(world.get_cells(), expected);
+}
+
+#[wasm_bindgen_test]
+fn test_conductor_become_electron_head_with_2_neighbours() {
+    let mut world = World::new(3, 3);
+    world.set_cells(&[
+        (1, 1, Cell::Conductor),
+        (0, 0, Cell::ElectronHead),
+        (0, 1, Cell::ElectronHead),
+    ]);
+    world.tick();
+
+    let expected = &[
+        Cell::ElectronTail, // 0 0
+        Cell::ElectronTail, // 0 1
+        Cell::Empty,        // 0 2
+        Cell::Empty,        // 1 0
+        Cell::ElectronHead, // 1 1
+        Cell::Empty,        // 1 2
+        Cell::Empty,        // 2 0
+        Cell::Empty,        // 2 1
+        Cell::Empty,        // 2 2
+    ];
+
+    assert_eq!(world.get_cells(), expected);
+}
+
+#[wasm_bindgen_test]
+fn test_conductor_not_become_electron_head_with_more_than_2_neighbours() {
+    let mut world = World::new(3, 3);
+    world.set_cells(&[
+        (1, 1, Cell::Conductor),
+        (0, 0, Cell::ElectronHead),
+        (0, 1, Cell::ElectronHead),
+        (0, 2, Cell::ElectronHead),
+    ]);
+    world.tick();
+
+    let expected = &[
+        Cell::ElectronTail, // 0 0
+        Cell::ElectronTail, // 0 1
+        Cell::ElectronTail, // 0 2
+        Cell::Empty,        // 1 0
+        Cell::Conductor,    // 1 1
+        Cell::Empty,        // 1 2
+        Cell::Empty,        // 2 0
+        Cell::Empty,        // 2 1
+        Cell::Empty,        // 2 2
+    ];
+
+    assert_eq!(world.get_cells(), expected);
+}
+
+#[cfg(test)]
+fn clock() -> World {
+    let mut world = World::new(64, 64);
+    world.set_cells(&[
+        (20, 20, Cell::Conductor),
+        (20, 21, Cell::Conductor),
+        (19, 22, Cell::Conductor),
+        (21, 22, Cell::Conductor),
+        (19, 21, Cell::Conductor),
+        (21, 21, Cell::Conductor),
+        (20, 23, Cell::Conductor),
+        (20, 24, Cell::Conductor),
+        (20, 25, Cell::Conductor),
+        (21, 25, Cell::Conductor),
+        (22, 25, Cell::Conductor),
+        (23, 25, Cell::Conductor),
+        (24, 25, Cell::Conductor),
+        (24, 24, Cell::Conductor),
+        (24, 23, Cell::Conductor),
+        (23, 22, Cell::Conductor),
+        (25, 22, Cell::Conductor),
+        (23, 21, Cell::Conductor),
+        (25, 21, Cell::Conductor),
+        (24, 22, Cell::Conductor),
+        (24, 20, Cell::Conductor),
+        (24, 19, Cell::Conductor),
+        (24, 18, Cell::Conductor),
+        (23, 18, Cell::Conductor),
+        (22, 18, Cell::ElectronHead),
+        (21, 18, Cell::Conductor),
+        (20, 18, Cell::Conductor),
+        (20, 19, Cell::Conductor),
+    ]);
+    world
+}
+
+#[wasm_bindgen_test]
+fn test_clock_9th_gen() {
+    let mut clock = clock();
+
+    clock.tick(); // 2nd gen
+    clock.tick(); // 3rd gen
+    clock.tick(); // 4th gen
+    clock.tick(); // 5th gen
+    clock.tick(); // 6th gen
+    clock.tick(); // 7th gen
+    clock.tick(); // 8th gen
+    clock.tick(); // 9th gen
+
+    let mut expected = World::new(64, 64);
+    expected.set_cells(&[
+        (20, 20, Cell::Conductor),
+        (20, 21, Cell::Conductor),
+        (19, 22, Cell::Conductor),
+        (21, 22, Cell::Conductor),
+        (19, 21, Cell::Conductor),
+        (21, 21, Cell::Conductor),
+        (20, 23, Cell::Conductor),
+        (20, 24, Cell::ElectronTail),
+        (20, 25, Cell::ElectronHead),
+        (21, 25, Cell::ElectronHead),
+        (22, 25, Cell::Conductor),
+        (23, 25, Cell::Conductor),
+        (24, 25, Cell::Conductor),
+        (24, 24, Cell::Conductor),
+        (24, 23, Cell::Conductor),
+        (23, 22, Cell::Conductor),
+        (25, 22, Cell::Conductor),
+        (23, 21, Cell::Conductor),
+        (25, 21, Cell::Conductor),
+        (24, 22, Cell::Conductor),
+        (24, 20, Cell::Conductor),
+        (24, 19, Cell::Conductor),
+        (24, 18, Cell::Conductor),
+        (23, 18, Cell::Conductor),
+        (22, 18, Cell::Conductor),
+        (21, 18, Cell::Conductor),
+        (20, 18, Cell::Conductor),
+        (20, 19, Cell::Conductor),
+    ]);
+
+    assert_eq!(&clock.get_cells(), &expected.get_cells());
+}
+
+#[wasm_bindgen_test]
+fn test_clock_10th_gen() {
+    let mut clock = clock();
+
+    clock.tick(); // 2nd gen
+    clock.tick(); // 3rd gen
+    clock.tick(); // 4th gen
+    clock.tick(); // 5th gen
+    clock.tick(); // 6th gen
+    clock.tick(); // 7th gen
+    clock.tick(); // 8th gen
+    clock.tick(); // 9th gen
+    clock.tick(); // 10th gen
+
+    let mut expected = World::new(64, 64);
+    expected.set_cells(&[
+        (20, 20, Cell::Conductor),
+        (20, 21, Cell::Conductor),
+        (19, 22, Cell::Conductor),
+        (21, 22, Cell::Conductor),
+        (19, 21, Cell::Conductor),
+        (21, 21, Cell::Conductor),
+        (20, 23, Cell::Conductor),
+        (20, 24, Cell::Conductor),
+        (20, 25, Cell::ElectronTail),
+        (21, 25, Cell::ElectronTail),
+        (22, 25, Cell::ElectronHead),
+        (23, 25, Cell::Conductor),
+        (24, 25, Cell::Conductor),
+        (24, 24, Cell::Conductor),
+        (24, 23, Cell::Conductor),
+        (23, 22, Cell::Conductor),
+        (25, 22, Cell::Conductor),
+        (23, 21, Cell::Conductor),
+        (25, 21, Cell::Conductor),
+        (24, 22, Cell::Conductor),
+        (24, 20, Cell::Conductor),
+        (24, 19, Cell::Conductor),
+        (24, 18, Cell::Conductor),
+        (23, 18, Cell::Conductor),
+        (22, 18, Cell::Conductor),
+        (21, 18, Cell::Conductor),
+        (20, 18, Cell::Conductor),
+        (20, 19, Cell::Conductor),
+    ]);
+
+    assert_eq!(&clock.get_cells(), &expected.get_cells());
+}
+
+#[wasm_bindgen_test]
+fn test_clock_11th_gen() {
+    let mut clock = clock();
+
+    clock.tick(); // 2nd gen
+    clock.tick(); // 3rd gen
+    clock.tick(); // 4th gen
+    clock.tick(); // 5th gen
+    clock.tick(); // 6th gen
+    clock.tick(); // 7th gen
+    clock.tick(); // 8th gen
+    clock.tick(); // 9th gen
+    clock.tick(); // 10th gen
+    clock.tick(); // 11th gen
+
+    let mut expected = World::new(64, 64);
+    expected.set_cells(&[
+        (20, 20, Cell::Conductor),
+        (20, 21, Cell::Conductor),
+        (19, 22, Cell::Conductor),
+        (21, 22, Cell::Conductor),
+        (19, 21, Cell::Conductor),
+        (21, 21, Cell::Conductor),
+        (20, 23, Cell::Conductor),
+        (20, 24, Cell::Conductor),
+        (20, 25, Cell::Conductor),
+        (21, 25, Cell::Conductor),
+        (22, 25, Cell::ElectronTail),
+        (23, 25, Cell::ElectronHead),
+        (24, 25, Cell::Conductor),
+        (24, 24, Cell::Conductor),
+        (24, 23, Cell::Conductor),
+        (23, 22, Cell::Conductor),
+        (25, 22, Cell::Conductor),
+        (23, 21, Cell::Conductor),
+        (25, 21, Cell::Conductor),
+        (24, 22, Cell::Conductor),
+        (24, 20, Cell::Conductor),
+        (24, 19, Cell::Conductor),
+        (24, 18, Cell::Conductor),
+        (23, 18, Cell::Conductor),
+        (22, 18, Cell::Conductor),
+        (21, 18, Cell::Conductor),
+        (20, 18, Cell::Conductor),
+        (20, 19, Cell::Conductor),
+    ]);
+
+    assert_eq!(&clock.get_cells(), &expected.get_cells());
 }
--- www/index.js
+++ www/index.js
@@ -1,3 +1,90 @@
-import * as wasm from "hello-wasm-pack";
+import { World, Cell, print_smth } from "wasm-wireworld-rs";
+import { memory } from "wasm-wireworld-rs/wasm_wireworld_rs_bg";
 
-wasm.greet();
+const CELL_SIZE = 10;
+const GRID_COLOR = "#EEEEEE";
+const EMPTY_COLOR = "#FFFFFF";
+const ELECTRONHEAD_COLOR = "#00ff00";
+const ELECTRONTAIL_COLOR = "#ff0000";
+const CONDUCTOR_COLOR = "#334257";
+
+const world = World.new(64, 64);
+
+const width = world.width();
+const height = world.height();
+
+const canvas = document.getElementById("wireworld-canvas");
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext('2d');
+
+const get_index = (row, column) => {
+    return row * width + column;
+};
+
+const drawGrid = () => {
+    ctx.beginPath();
+    ctx.strokeStyle = GRID_COLOR;
+
+    for (let i = 0; i < width; i++) {
+        ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+        ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+    }
+
+    for (let j = 0; j < height; j++) {
+        ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+        ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1)
+    }
+
+    ctx.stroke();
+}
+
+const drawCells = () => {
+    const cellsPtr = world.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+    ctx.beginPath();
+
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = get_index(row, col);
+
+            switch (cells[idx]) {
+                case Cell.Empty:
+                    ctx.fillStyle = EMPTY_COLOR;
+                    break;
+                case Cell.ElectronHead:
+                    ctx.fillStyle = ELECTRONHEAD_COLOR;
+                    break;
+                case Cell.ElectronTail:
+                    ctx.fillStyle = ELECTRONTAIL_COLOR;
+                    break;
+                case Cell.Conductor:
+                    ctx.fillStyle = CONDUCTOR_COLOR;
+                    break;
+            }
+
+            ctx.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
+}
+
+
+drawGrid();
+drawCells();
+
+/*
+window.setInterval(() => {
+    
+    world.tick();
+    
+    drawGrid();
+    drawCells();
+}, 1000)
+*/
